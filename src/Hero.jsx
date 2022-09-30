14397705c25b303fa1acbaccb2a7754276456df2
Alfredosang <osangalfred@gmail.com>
2022-09-30 04:58:38 -0700

    update

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,17 +4,17 @@ import brandpic from './Assets/brandpic.png'
 const Hero = () => {
     return (
         <div>
-            <div className='container flex flex-col pt-24 md:flex-row justify-center items-center space-x-1 px-24 mx-auto'>
+            <div className='container flex flex-col pt-24 md:flex-row justify-center items-center  px-24 mx-auto'>
                 <div >
-                    
-                    <img src={brandpic} alt='Brand Pic' className='w-5/5  hover:scale-105 duration-300 '></img>
+
+                    <img src={brandpic} alt='Brand Pic' className='w-[1000px]  hover:scale-105 duration-300 object-cover object-center '></img>
 
                 </div>
                 <div>
                     <h2 className='text-white font-bold text-3xl md:text-5xl mt-3 mb-3 py-3 md:leading-[55px] hover:scale-105 duration-200'>Hello my name is  Alfred Osang, I'm a Frontend Developer. </h2>
 
-                <p className='text-gray-300 md:text-2xl hover:scale-105 duration-200 '>I develop problem solving web apps, for small and large scale businesses...</p>
-                    
+                    <p className='text-gray-300 md:text-2xl hover:scale-105 duration-200 '>I develop problem solving web apps, for small and large scale businesses...</p>
+
 
                 </div>
             </div>
