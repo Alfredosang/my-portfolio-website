8f11b004feefda8520c87742969b29ea25e6afd7
Alfredosang <osangalfred@gmail.com>
2022-10-29 12:24:01 -0700

    about me update

--- src/About.jsx
+++ src/About.jsx
@@ -11,18 +11,11 @@ const About = () => {
                 
                 <div>
 
-                    <p className='text-white text-xl '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
-                        natus quaerat quae earum.  Quos beatae impedit aut, odio maxime nemo neque doloremque,
-                        eius iure eum id autem accusamus
-                        optio aspernatur provident non et ratione, officia a minima velit numquam. Sed assumenda
-                        molestias voluptate voluptatibus dolore earum, unde dolor alias animi? Lorem ipsum dolor sit
-                        amet consectetur adipisicing elit. Quod error neque tenetur id eligendi nihil alias cum ipsam
-                        voluptates nesciunt voluptatibus repellendus, voluptas deleniti eos repellat sequi blanditiis
-                        vitae accusamus possimus sit commodi, beatae atque provident. Tempora laudantium maxime dolorum
-                        veritatis enim, molestiae quod eius aliquid laboriosam deserunt, neque consequuntur id corrupti voluptatum.
-                        Nam odio similique minus saepe, laborum perferendis magnam, odit est repellendus vero dolor quis accusantium
-                        amet porro! Laborum iste ipsum mollitia quibusdam in libero officia, reiciendis laboriosam autem molestias et.
-                        Optio laborum incidunt officiis consequuntur cumque, nam sit. Ullam nobis labore, deserunt ab distinctio minus reprehenderit quis.</p>
+                    <p className='text-white sm:text-2xl sm:p-16 '>
+                    I have a B.Sc. in computer science and am a certified React developer.
+                     I create reusable, scalable, and optimized client-side features, structures,
+                      and designs for web pages to improve user experience.
+                    </p>
                 </div>
 
 
